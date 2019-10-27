e9f67b42ce7c724f44ee1e77fd3ab673be3efc35
Viktor Lidholt <viktorlidholt@gmail.com>
2019-10-27 19:28:01 +0100

    Comments in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:device_simulator/device_simulator.dart';
 
+// It's good practice to define a constant for enabling the device simulator
+// so you can easily turn it on or off
 const bool debugEnableDeviceSimulator = true;
 
 void main() => runApp(MyApp());
 
+// Insert Device simulator at the top of your widget, as a child of your
+// App widget. Build the rest of your widget tree as you would normally do.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
