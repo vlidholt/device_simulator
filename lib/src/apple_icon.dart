9e325c4263ea1a4f875afe6d000b9b64db9f0eb6
Chris D <chris@venngo.com>
2020-06-16 00:50:55 -0400

    fixes a problem with Flutter (Channel beta, 1.19.0-4.1.pre, on Mac OS X 10.15.5 19F101, locale en-CA), with error: Target release_ios_bundle_flutter_assets failed: Exception: Avoid non-constant invocations of IconData or try to build again with --no-tree-shake-icons.

--- lib/src/apple_icon.dart
+++ lib/src/apple_icon.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class IconApple {
-  static final apple = IconData(
+  static final apple = const IconData(
     0xeabe,
     fontFamily: 'appleicon',
     fontPackage: 'device_simulator',
