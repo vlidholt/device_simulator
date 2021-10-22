d745943c236da939f4d8a2d6fe8fec4ff1591ee2
Peter Ullrich <dotup.software@gmail.com>
2021-10-22 13:26:34 +0200

    new devices

--- lib/src/device_specification.dart
+++ lib/src/device_specification.dart
@@ -11,14 +11,41 @@ class DeviceSpecification {
   final double navBarHeight;
   final TargetPlatform platform;
 
-  DeviceSpecification(
-      {required this.name,
-      required this.size,
-      required this.padding,
-      required this.platform,
-      this.paddingLandscape,
-      this.cornerRadius = 0.0,
-      this.notchSize,
-      this.tablet = false,
-      this.navBarHeight = 48.0});
+  DeviceSpecification({
+    required this.name,
+    required this.size,
+    required this.padding,
+    required this.platform,
+    this.paddingLandscape,
+    this.cornerRadius = 0.0,
+    this.notchSize,
+    this.tablet = false,
+    this.navBarHeight = 48.0,
+  });
+
+  factory DeviceSpecification.fromPixel({
+    required double height,
+    required double width,
+    required double pixelRatio,
+    required EdgeInsets padding,
+    EdgeInsets? paddingLandscape,
+    required String name,
+    double cornerRadius = 0.0,
+    Size? notchSize,
+    bool tablet = false,
+    double navBarHeight = 48,
+    required TargetPlatform platform,
+  }) {
+    return DeviceSpecification(
+      name: name,
+      padding: padding,
+      platform: platform,
+      size: Size(width / pixelRatio, height / pixelRatio),
+      cornerRadius: cornerRadius,
+      navBarHeight: navBarHeight,
+      notchSize: notchSize,
+      paddingLandscape: paddingLandscape,
+      tablet: tablet,
+    );
+  }
 }
